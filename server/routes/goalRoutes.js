989ccadacdda34fa069c5abc6c4fcdd564268e1b
Tim Butts <timbuttsiii@gmail.com>
2023-03-01 14:02:50 -0500

    Added Frontend UI

--- server/routes/goalRoutes.js
+++ server/routes/goalRoutes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const { getGoals, deleteGoal, updateGoal, setGoal} = require('../controllers/goalController')
 const {get} = require("mongoose");
+const {protect} = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(setGoal)
-router.route('/:id').put(updateGoal).delete(deleteGoal)
+router.route('/').get(protect, getGoals).post(protect,setGoal)
+router.route('/:id').put(protect, updateGoal).delete(protect, deleteGoal)
 
 module.exports = router
